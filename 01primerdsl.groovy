f82054b32aa5e80d6dfdbae347bec7722373d3a2
Edilberto Mejia Flores <edilberto.mejia@laureate.pe>
2024-05-15 11:07:45 -0500

    inicial

---
+++ 01primerdsl.groovy
@@ -0,0 +1,21 @@
+job('ejemplo-job-DSL') {
+  description('Job DSL de ejemplo para el curso de Jenkins')
+  scm {
+    git('https://github.com/edisaac/jenkins.job.parametrizado.git', 'main') { node ->
+      node / gitConfigName('edisaac')
+      node / gitConfigEmail('edisaac.mejia@gmail.com')
+    }
+  } 
+  parameters {
+    stringParam('nombre', defaultValue = 'Julian', description = 'Parametro de cadena para el Job Booleano')
+    choiceParam('planeta', ['Mercurio', 'Venus', 'Tierrra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
+    booleanParam('agente', false)
+  }
+  triggers {
+    cron('H/7 * * * *')
+  }
+  steps {
+    shell("bash jobscript.sh")
+  }
+   
+}
---
+++ readme.md
@@ -0,0 +1,20 @@
+ver
+
+https://jenkinsci.github.io/job-dsl-plugin/#
+
+
+Crear un seed JOB
+
+Panel de Control>Todo>Nuevo Tarea
+
+name: job dsl 1
+type: proyecto libre
+
+
+Ir a opcion ejecutar
+BUILD STEPS : Process Job DSLs
+Use the provided DSL script
+
+Agregar el contenido de 01primerdsl.groovy
+
+
